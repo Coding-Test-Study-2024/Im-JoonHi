f00531f377fb5c7ce0c5fad027a4bff3673a66dd
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-23 11:20:34 +0900

    file : 1주차 파일 추가

---
+++ Week-01/Fibonacci_numbers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class Fibonacci_numbers
+    {
+        public class Solution
+        {
+            public int fivonacci(int loop, int prev, int now)
+            {
+                if (loop == 0) return prev;
+                return fivonacci(loop - 1, now % 1234567, (prev + now) % 1234567);
+            }
+
+            public int solution(int n)
+            {
+                int answer = fivonacci(n, 0, 1);
+                return answer;
+            }
+        }
+    }
+}
