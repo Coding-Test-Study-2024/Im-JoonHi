2c991e6f00873eb411d7b4eb1a8a494bccbbf09b
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-25 17:36:59 +0900

    feat : 코테 치킨 TOP N (BOJ-11582) 해답 추가

--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace CodingTest
+﻿using System.Text;
+
+namespace CodingTest
 {
     internal class Program
     {
@@ -6,7 +8,7 @@
         {
             Console.WriteLine("Hello, World!");
 
-            /*
+            /* NQWEEN
             Console.Write("NQween 입력 : ");
             int input = int.Parse(Console.ReadLine());
             int[] qweens = new int[input];
@@ -16,6 +18,18 @@
             Console.WriteLine("///");
             Console.WriteLine(result);
             */
+
+            // ChickenTopN
+            string input = Console.ReadLine();
+            int N = int.Parse(input);
+            List<int> grade = new List<int>(N);
+            input = Console.ReadLine();
+            for (int i = 0; i < N; i++)
+                grade.Add(int.Parse((input.Split(" "))[i]));
+            input = Console.ReadLine();
+            int K = int.Parse(input);
+            ChickenTopN.ChickenTOPN(K, N, ref grade);
+            Console.WriteLine(string.Join(' ', grade));
         }
     }
 }
