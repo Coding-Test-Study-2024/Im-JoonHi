541246102227f8da8923cf6fe0352427413d1957
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-01-27 15:38:00 +0900

    chore : 파일 추가 삭제

---
+++ Week-01/ChickenTopN.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class ChickenTopN
+    {
+        public static void ChickenTOPN(int K, int N, ref List<int> grade) 
+        {
+            int now = 0;
+
+            for (int i = 0; i < K; i++)
+            {
+                grade.Sort(now, N / K, null);
+                now += N / K;
+            }
+        }
+    }
+}
