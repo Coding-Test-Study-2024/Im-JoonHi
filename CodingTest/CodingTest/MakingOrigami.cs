357af136b51f1cd7b2e0495dbdad8ced96119f95
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-06 11:20:45 +0900

    feat : 코테 색종이 만들기 해답 추가

--- CodingTest/CodingTest/MakingOrigami.cs
+++ CodingTest/CodingTest/MakingOrigami.cs
@@ -9,8 +9,35 @@ namespace CodingTest
     // BOJ-2630 색종이 만들기
     internal class MakingOrigami
     {
-        public void origami(int[,]paper, int n, ref int[] output) 
+        public static void origami(bool[,] paper, int n, int x, int y, ref int[] output) 
         {
+            bool color = paper[y, x];
+            bool isSame = true;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                {
+                    if (paper[y + i, x + j] != color)
+                    {
+                        isSame = false;
+                        break;
+                    }
+                if (!isSame) break;
+                }
+            if (!isSame)
+            {
+                int start = n / 2;
+                origami(paper, start, x, y, ref output);
+                origami(paper, start, x + start, y, ref output);
+                origami(paper, start, x, y + start, ref output);
+                origami(paper, start, x + start, y + start, ref output);
+            }
+            else
+            {
+                if (color == false)
+                    output[0] += 1;
+                else
+                    output[1] += 1;
+            }
         }
     }
 }
--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -7,6 +7,7 @@ namespace CodingTest
 {
     internal class Program
     {
+        /*
         static void Main(string[] args)
         {
             string num = Console.ReadLine();
@@ -22,6 +23,28 @@ namespace CodingTest
                     ints[i, j] = int.Parse(Buf[j]);
             }
             Console.WriteLine(SpecialAward.specialAward(ints, n, 0, 0));
+        }
+        */
+
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int n = int.Parse(num);
+            bool[,] paper = new bool[n, n];
+
+            string input;
+            for (int i = 0; i < n; i++)
+            {
+                input = Console.ReadLine();
+                string[] Buf = input.Split(' ');
+                for (int j = 0; j < n; j++)
+                    paper[i, j] = Buf[j] is "1" ? true : false;
+            }
+
+            int[] output = new int[2] { 0, 0 };
+            MakingOrigami.origami(paper, n, 0, 0, ref output);
+            foreach(int result in output)
+                Console.WriteLine(result);
         }
     }
 }
