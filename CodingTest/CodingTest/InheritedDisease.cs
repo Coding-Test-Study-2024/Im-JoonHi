ed1855e6c504a131e8eb237bf17f746906877528
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-07 08:03:34 +0900

    feat : 코테 InheritedDisease 푸는중

--- CodingTest/CodingTest/InheritedDisease.cs
+++ CodingTest/CodingTest/InheritedDisease.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,9 +12,24 @@ namespace CodingTest
 
     internal class InheritedDisease
     {
-        public static void inheriteddisese(Queue<int> D, int result)
+        public static void inheriteddisese(Queue<int> D, Queue<int> input, int lastMax,int lastNode, int Gen, int nodenum)
         {
-            
+            if (input.Count == 0)
+                return;
+            if (lastMax == -1)
+            {
+                D.Enqueue(1);
+                input.Dequeue();
+                inheriteddisese(D, input, 1, 1, 2, 2);
+                return;
+            }
+            // int thisGenNodenum = nodenum * Gen; // 이번에 제작할 노드 총 갯수
+            // int thisGenSelect = (nodenum + lastNode - lastMax - 1) * Gen + input.Dequeue();
+            int NodeSelect = input.Dequeue(); // 이번에 선택될 노드 번지
+            Console.WriteLine((nodenum - (lastMax - lastNode) - 1) * Gen + NodeSelect);
+            D.Enqueue(lastMax + (nodenum - (lastMax - lastNode) - 1) * Gen + NodeSelect);
+            inheriteddisese(D, input, nodenum * (Gen+1), lastMax + (nodenum - (lastMax - lastNode) - 1) * Gen + NodeSelect, Gen + 1 , nodenum * Gen);
+            return;
         }
     }
 }
--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -7,7 +7,26 @@ namespace CodingTest
 {
     internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            int n = int.Parse(num);
+
+            Queue<int> ints = new Queue<int>(n);
+            string input = Console.ReadLine();
+            string[] Buf = input.Split(' ');
+            for (int i = 0; i < n; i++)
+                ints.Enqueue(int.Parse(Buf[i]));
+
+            Queue<int> D = new Queue<int>(n);
+            InheritedDisease.inheriteddisese(D, ints, -1, 0, 0, 0);
+
+            for (int i = 0; i < n; i++)
+                Console.Write($"{D.Dequeue()} ");
+        }
+
+        //특별상이라도 받고 싶어
+        /*
         static void Main(string[] args)
         {
             string num = Console.ReadLine();
@@ -24,7 +43,8 @@ namespace CodingTest
             }
             Console.WriteLine(SpecialAward.specialAward(ref ints, n, 0, 0));
         }
-        
+        */
+
 
         // 색종이 만들기
         /*
