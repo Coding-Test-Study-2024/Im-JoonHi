32e7b8bec81ae8a251585a4026a0ce89f7ed7a1c
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-07 17:26:57 +0900

    feat : 코테 푸는중...

--- CodingTest/CodingTest/InheritedDisease.cs
+++ CodingTest/CodingTest/InheritedDisease.cs
@@ -1,34 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Numerics;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodingTest
+﻿namespace CodingTest
 {
     // (a + b + c + d) % e
     // (a%e + b%e + c%e + d%e) % e
 
     internal class InheritedDisease
     {
-        public static void inheriteddisese(Queue<int> D, Queue<int> input, int lastMax,int lastNode, int Gen, int nodenum)
+        static long P = 1000000007;
+
+        public static void inheriteddisese(Queue<int> D, Queue<int> input, int lastMax, int lastNode, int Gen, int nodenum)
         {
             if (input.Count == 0)
                 return;
             if (lastMax == -1)
             {
                 D.Enqueue(1);
-                input.Dequeue();
-                inheriteddisese(D, input, 1, 1, 2, 2);
+                inheriteddisese(D, input, 1, 1, 1, 1);
                 return;
             }
             // int thisGenNodenum = nodenum * Gen; // 이번에 제작할 노드 총 갯수
             // int thisGenSelect = (nodenum + lastNode - lastMax - 1) * Gen + input.Dequeue();
             int NodeSelect = input.Dequeue(); // 이번에 선택될 노드 번지
-            Console.WriteLine((nodenum - (lastMax - lastNode) - 1) * Gen + NodeSelect);
-            D.Enqueue(lastMax + (nodenum - (lastMax - lastNode) - 1) * Gen + NodeSelect);
-            inheriteddisese(D, input, nodenum * (Gen+1), lastMax + (nodenum - (lastMax - lastNode) - 1) * Gen + NodeSelect, Gen + 1 , nodenum * Gen);
+            Console.WriteLine();
+            D.Enqueue(lastNode + ((nodenum - (lastMax - lastNode ) - 1) * (Gen+1) + NodeSelect));
+            Console.WriteLine(NodeSelect);
+            Console.WriteLine(lastNode + ((nodenum - (lastMax - lastNode) - 1) * (Gen + 1)));
+            Console.WriteLine($"{nodenum * (Gen + 1) + lastMax} {lastNode + ((nodenum - (lastMax - lastNode) - 1) * (Gen + 1) + NodeSelect)} {Gen + 1} {nodenum * (Gen + 1)}");
+            inheriteddisese(D, input, nodenum * (Gen + 1) + lastMax, lastNode + ((nodenum - (lastMax - lastNode) - 1) * (Gen + 1) + NodeSelect), Gen + 1, nodenum * (Gen + 1));
+            // 3 [2] 2 2
             return;
         }
     }
