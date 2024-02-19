9eb52c322ee0110a4d728d88efec03b962d608cd
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-20 01:36:07 +0900

    feat : 조이스틱 푸는중

--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -1,9 +1,22 @@
 ﻿using System.Text;
+using System.Collections.Generic;
 
 namespace CodingTest
 {
     internal class Program
     {
-        
+        static void Main(string[] args)
+        {
+            int answer = 0;
+
+            string name = "JEROEN";
+            List<char> names = new List<char>(name.Length);
+            foreach (char n in name)
+                names.Add(n);
+            foreach (char n in names)
+            {
+                Console.WriteLine(Convert.ToInt32(n)-64);
+            }
+        }
     }
 }
