de911ee8bcfc0e5caf1f5e5f34fb53a4fad29599
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-25 15:49:47 +0900

    feat : 코테 BOJ-2606 바이러스 해답 추가

--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -7,16 +7,37 @@ namespace CodingTest
     {
         static void Main(string[] args)
         {
-            int answer = 0;
+            string num = Console.ReadLine();
+            int n = int.Parse(num);
+            bool[,] graph = new bool[n,n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    graph[i, j] = false;
 
-            string name = "JEROEN";
-            List<char> names = new List<char>(name.Length);
-            foreach (char n in name)
-                names.Add(n);
-            foreach (char n in names)
+            num = Console.ReadLine();
+            n = int.Parse(num);
+
+            for (int i = 0; i < n; i++)
             {
-                Console.WriteLine(Convert.ToInt32(n)-64);
+                string input = Console.ReadLine();
+                string[] Buf = input.Split(' ');
+                graph[(int.Parse(Buf[0]))-1, (int.Parse(Buf[1]))-1] = true;
+                graph[(int.Parse(Buf[1]))-1, (int.Parse(Buf[0]))-1] = true;
             }
+
+            bool[] bfsVisited;
+            Virus.virus(in graph, 0, out bfsVisited);
+            Console.WriteLine(PrintGraphSearch(bfsVisited));
+        }
+
+        private static int PrintGraphSearch(bool[] visited)
+        {
+            int visit = -1;
+            for (int i = 0; i < visited.Length; i++)
+            {
+                if (visited[i] == true) visit++;
+            }
+            return visit;
         }
     }
 }
---
+++ CodingTest/CodingTest/Virus.cs
@@ -0,0 +1,35 @@
+﻿namespace CodingTest
+{
+    // BOJ-2606 바이러스
+    internal class Virus
+    {
+        public static void virus(in bool[,] graph, int start, out bool[] visited)
+        {
+            visited = new bool[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+            }
+            visited[start] = true;
+
+            Queue<int> bfsQueue = new Queue<int>();
+
+            bfsQueue.Enqueue(start);
+            while (bfsQueue.Count > 0)
+            {
+                int next = bfsQueue.Dequeue();
+
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[next, i] &&
+                        !visited[i])
+                    {
+                        visited[i] = true;
+                        bfsQueue.Enqueue(i);
+                    }
+                }
+            }
+        }
+    }
+}
