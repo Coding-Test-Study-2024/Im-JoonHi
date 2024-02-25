487b0d1a54bb07e4d854a07d5f176624411e672e
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-25 18:25:46 +0900

    feat : 코테 DFS와 BFS 추가

---
+++ CodingTest/CodingTest/DFSandBFS.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class DFSandBFS
+    {
+        public static void DFS(in bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+
+            SearchNode(graph, start, visited, parents);
+        }
+
+        private static void SearchNode(in bool[,] graph, int start, bool[] visited, int[] parents)
+        {
+            visited[start] = true;
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                if (graph[start, i] &&
+                    !visited[i])
+                {
+                    parents[i] = start;
+                    SearchNode(graph, i, visited, parents);
+                }
+            }
+        }
+
+        public static void BFS(in bool[,] graph, int start, out bool[] visited, out int[] parents)
+        {
+            visited = new bool[graph.GetLength(0)];
+            parents = new int[graph.GetLength(0)];
+
+            for (int i = 0; i < graph.GetLength(0); i++)
+            {
+                visited[i] = false;
+                parents[i] = -1;
+            }
+            visited[start] = true;
+
+            Queue<int> bfsQueue = new Queue<int>();
+
+            bfsQueue.Enqueue(start);
+            while (bfsQueue.Count > 0)
+            {
+                int next = bfsQueue.Dequeue();
+
+                for (int i = 0; i < graph.GetLength(0); i++)
+                {
+                    if (graph[next, i] &&
+                        !visited[i])
+                    {
+                        visited[i] = true;
+                        parents[i] = next;
+                        bfsQueue.Enqueue(i);
+                    }
+                }
+            }
+        }
+    }
+}
--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -5,6 +5,8 @@ namespace CodingTest
 {
     internal class Program
     {
+        /*
+        // BOJ-2606 바이러스
         static void Main(string[] args)
         {
             string num = Console.ReadLine();
@@ -38,6 +40,53 @@ namespace CodingTest
                 if (visited[i] == true) visit++;
             }
             return visit;
+        }
+        */
+
+        static void Main(string[] args)
+        {
+            string num = Console.ReadLine();
+            string[] Buf = num.Split(' ');
+            int n = int.Parse(Buf[0]);
+            int start = int.Parse(Buf[2]);
+            bool[,] graph = new bool[n, n];
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    graph[i, j] = false;
+
+            n = int.Parse(Buf[1]);
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                string[] Buf2 = input.Split(' ');
+                graph[(int.Parse(Buf2[0])) - 1, (int.Parse(Buf2[1])) - 1] = true;
+                graph[(int.Parse(Buf2[1])) - 1, (int.Parse(Buf2[0])) - 1] = true;
+            }
+
+            // DFS 탐색
+            bool[] dfsVisited;
+            int[] dfsPath;
+            DFSandBFS.DFS(in graph, start, out dfsVisited, out dfsPath);
+            Console.WriteLine("<DFS>");
+            PrintGraphSearch(dfsVisited, dfsPath);
+            Console.WriteLine();
+
+            // BFS 탐색
+            bool[] bfsVisited;
+            int[] bfsPath;
+            DFSandBFS.BFS(in graph, start, out bfsVisited, out bfsPath);
+            Console.WriteLine("<BFS>");
+            PrintGraphSearch(bfsVisited, bfsPath);
+            Console.WriteLine();
+        }
+        private static void PrintGraphSearch(bool[] visited, int[] path)
+        {
+            Console.WriteLine($"{"Vertex",8}{"Visit",8}{"Path",8}");
+
+            for (int i = 0; i < visited.Length; i++)
+            {
+                Console.WriteLine($"{i,8}{visited[i],8}{path[i],8}");
+            }
         }
     }
 }
