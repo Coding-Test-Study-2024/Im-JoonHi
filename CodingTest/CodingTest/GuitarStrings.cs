1ba7a21dda384f72caabf7b9ee243e3b3ee66f4c
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-02-29 17:53:17 +0900

    Create GuitarStrings.cs

---
+++ CodingTest/CodingTest/GuitarStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    // BOJ - 1049
+    internal class GuitarStrings
+    {
+        public static int stringCount()
+        {
+            return 0;
+        }
+    }
+}
