6d64f17bd98d2bc5ae87fb94ab511a8fbe408582
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-03-24 18:27:48 +0900

    feat : 코테 BOJ-1026 보물, BOJ-1049 기타줄 해답 추가

--- CodingTest/CodingTest/Activate_code.cs
+++ CodingTest/CodingTest/Activate_code.cs
@@ -178,6 +178,101 @@ namespace CodingTest
        }
        */
 
+            /* 5주차
+            // BOJ-2606 바이러스
+            static void Main(string[] args)
+            {
+                string num = Console.ReadLine();
+                int n = int.Parse(num);
+                bool[,] graph = new bool[n,n];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        graph[i, j] = false;
+
+                num = Console.ReadLine();
+                n = int.Parse(num);
+
+                for (int i = 0; i < n; i++)
+                {
+                    string input = Console.ReadLine();
+                    string[] Buf = input.Split(' ');
+                    graph[(int.Parse(Buf[0]))-1, (int.Parse(Buf[1]))-1] = true;
+                    graph[(int.Parse(Buf[1]))-1, (int.Parse(Buf[0]))-1] = true;
+                }
+
+                bool[] bfsVisited;
+                Virus.virus(in graph, 0, out bfsVisited);
+                Console.WriteLine(PrintGraphSearch(bfsVisited));
+            }
+
+            private static int PrintGraphSearch(bool[] visited)
+            {
+                int visit = -1;
+                for (int i = 0; i < visited.Length; i++)
+                {
+                    if (visited[i] == true) visit++;
+                }
+                return visit;
+            }
+
+            // BOJ-1260 DFS와 BFS
+            static void Main(string[] args)
+            {
+                string num = Console.ReadLine();
+                string[] Buf = num.Split(' ');
+                int n = int.Parse(Buf[0]);
+                int start = int.Parse(Buf[2]);
+                bool[,] graph = new bool[n, n];
+                for (int i = 0; i < n; i++)
+                    for (int j = 0; j < n; j++)
+                        graph[i, j] = false;
+
+                n = int.Parse(Buf[1]);
+                for (int i = 0; i < n; i++)
+                {
+                    string input = Console.ReadLine();
+                    string[] Buf2 = input.Split(' ');
+                    graph[(int.Parse(Buf2[0])) - 1, (int.Parse(Buf2[1])) - 1] = true;
+                    graph[(int.Parse(Buf2[1])) - 1, (int.Parse(Buf2[0])) - 1] = true;
+                }
+
+                bool[] Visited = new bool[graph.GetLength(0)];
+                Array.Fill(Visited, false);
+                // DFS 탐색
+                bool[] dfsVisited;
+                List<int> dfsPath;
+                DFSandBFS.DFS(in graph, start-1, out Visited, out dfsPath);
+                foreach (int numbers in dfsPath)
+                    Console.Write($"{numbers} ");
+                Console.WriteLine();
+
+                // BFS 탐색
+                Array.Fill(Visited, false);
+                List<int> bfsPath;
+                DFSandBFS.BFS(in graph, start-1, out Visited, out bfsPath);
+                foreach (int numbers in bfsPath)
+                    Console.Write($"{numbers} ");
+                Console.WriteLine();
+            }
+
+            // BOJ-2210 숫자판 점프
+            static void Main(string[] args)
+            {
+                List<string> board = new List<string>(25);
+                for (int i = 0; i < 5; i++)
+                {
+                    string input = Console.ReadLine();
+                    string[] Buf = input.Split(' ');
+                    board.Add(Buf[0]);
+                    board.Add(Buf[1]);
+                    board.Add(Buf[2]);
+                    board.Add(Buf[3]);
+                    board.Add(Buf[4]);
+                }
+                Console.Write(NumberBoardJump.numboardjump(board.ToArray()));
+            }
+            */
+
             /* 번외
 
             /* NQWEEN
--- CodingTest/CodingTest/Program.cs
+++ CodingTest/CodingTest/Program.cs
@@ -1,108 +1,54 @@
-﻿using System.Text;
-using System.Collections.Generic;
-
-namespace CodingTest
+﻿namespace CodingTest
 {
     internal class Program
     {
-        // 5주차
+        // 8주차
+
         /*
-        // BOJ-2606 바이러스
+        // BOJ-1026 보물
         static void Main(string[] args)
         {
             string num = Console.ReadLine();
             int n = int.Parse(num);
-            bool[,] graph = new bool[n,n];
+            List<int> input1 = new List<int>(n);
+            List<int> input2 = new List<int>(n);
+
+            string input;
+            input = Console.ReadLine();
+            string[] buf = input.Split(" ");
             for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    graph[i, j] = false;
+                input1.Add(int.Parse(buf[i]));
 
-            num = Console.ReadLine();
-            n = int.Parse(num);
-
+            input = Console.ReadLine();
+            buf = input.Split(" ");
             for (int i = 0; i < n; i++)
-            {
-                string input = Console.ReadLine();
-                string[] Buf = input.Split(' ');
-                graph[(int.Parse(Buf[0]))-1, (int.Parse(Buf[1]))-1] = true;
-                graph[(int.Parse(Buf[1]))-1, (int.Parse(Buf[0]))-1] = true;
-            }
+                input2.Add(int.Parse(buf[i]));
 
-            bool[] bfsVisited;
-            Virus.virus(in graph, 0, out bfsVisited);
-            Console.WriteLine(PrintGraphSearch(bfsVisited));
+            input1.Sort(); input2.Sort();
+            Console.WriteLine(Treasure.treasure(ref input1, ref input2));
         }
-
-        private static int PrintGraphSearch(bool[] visited)
-        {
-            int visit = -1;
-            for (int i = 0; i < visited.Length; i++)
-            {
-                if (visited[i] == true) visit++;
-            }
-            return visit;
-        }
-        */
-
-        /*
-        // BOJ-1260 DFS와 BFS
+        
+        // BOJ-1049 기타줄
         static void Main(string[] args)
         {
             string num = Console.ReadLine();
-            string[] Buf = num.Split(' ');
-            int n = int.Parse(Buf[0]);
-            int start = int.Parse(Buf[2]);
-            bool[,] graph = new bool[n, n];
-            for (int i = 0; i < n; i++)
-                for (int j = 0; j < n; j++)
-                    graph[i, j] = false;
-
-            n = int.Parse(Buf[1]);
-            for (int i = 0; i < n; i++)
+            string[] buf = num.Split(" ");
+            int n = int.Parse(buf[0]);
+            int m = int.Parse(buf[1]);
+            List<int> input1 = new List<int>(m);
+            List<int> input2 = new List<int>(m);
+            for (int i = 0; i < m; i++)
             {
-                string input = Console.ReadLine();
-                string[] Buf2 = input.Split(' ');
-                graph[(int.Parse(Buf2[0])) - 1, (int.Parse(Buf2[1])) - 1] = true;
-                graph[(int.Parse(Buf2[1])) - 1, (int.Parse(Buf2[0])) - 1] = true;
+                num = Console.ReadLine();
+                buf = num.Split(" ");
+                input1.Add(int.Parse(buf[0]));
+                input2.Add(int.Parse(buf[1]));
             }
-
-            bool[] Visited = new bool[graph.GetLength(0)];
-            Array.Fill(Visited, false);
-            // DFS 탐색
-            bool[] dfsVisited;
-            List<int> dfsPath;
-            DFSandBFS.DFS(in graph, start-1, out Visited, out dfsPath);
-            foreach (int numbers in dfsPath)
-                Console.Write($"{numbers} ");
-            Console.WriteLine();
-
-            // BFS 탐색
-            Array.Fill(Visited, false);
-            List<int> bfsPath;
-            DFSandBFS.BFS(in graph, start-1, out Visited, out bfsPath);
-            foreach (int numbers in bfsPath)
-                Console.Write($"{numbers} ");
-            Console.WriteLine();
+            Console.WriteLine(GuitarStrings.stringCount(n, ref input1, ref input2));
         }
         */
-
-        
-        // BOJ-2210 숫자판 점프
+        //BOJ-1052 물병
         static void Main(string[] args)
         {
-            List<string> board = new List<string>(25);
-            for (int i = 0; i < 5; i++)
-            {
-                string input = Console.ReadLine();
-                string[] Buf = input.Split(' ');
-                board.Add(Buf[0]);
-                board.Add(Buf[1]);
-                board.Add(Buf[2]);
-                board.Add(Buf[3]);
-                board.Add(Buf[4]);
-            }
-            Console.Write(NumberBoardJump.numboardjump(board.ToArray()));
         }
-        
-    }
 }
