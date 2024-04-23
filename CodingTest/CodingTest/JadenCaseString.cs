c457755ac4e1a1152eef98f36bed1b6fccf19c67
임준희 <95527079+TWE1VE20@users.noreply.github.com>
2024-04-23 22:04:05 +0900

    feat : 코테 PGM-12951 JadenCase 문자열 만들기 해답 추가

---
+++ CodingTest/CodingTest/JadenCaseString.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class JadenCaseString
+    {
+        public string solution(string s)
+        {
+            char[] chars = s.ToLower().ToCharArray();
+            chars[0] = char.ToUpper(chars[0]);
+            for (int i = 1; i < chars.Length; i++)
+                if (chars[i - 1] == ' ')
+                    chars[i] = char.ToUpper(chars[i]);
+            return string.Join(string.Empty, chars);
+        }
+    }
+}
