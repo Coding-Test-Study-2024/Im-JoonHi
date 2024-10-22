30c9778e38e3364e31360c5e4df15301939e7c66
TWE1VE20 <95527079+TWE1VE20@users.noreply.github.com>
2024-10-22 23:18:26 +0900

    Create HashClothes.cs

---
+++ Hash/HashClothes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingTest
+{
+    internal class HashClothes
+    {
+        public class Solution
+        {
+            public int solution( string [,] clothes )
+            {
+                int answer = 0;
+                return answer;
+            }
+        }
+    }
+}
